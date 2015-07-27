eedf4db9cba416df8be530d255106e9dbdb34d4b
Kayleigh Migdol <kayleigh@migdol.net>
2015-07-27 15:16:53 -0400

    Adds game.js and constructor for Entity class

---
+++ game.js
@@ -0,0 +1,9 @@
+//Constructor for Entitiy Class
+function Entity (w,h,m,xPos,yPos,img) {
+	this.width = w;
+	this.height = h;
+	this.canMoveOnto = m;
+	this.x = xPos;
+	this.y = yPos;
+	this.imageSource = img;
+}
