ec6bed8d057dd1f69e377f33bfc2e625c97a331b
Kayleigh Migdol <kayleigh@migdol.net>
2015-07-27 16:02:31 -0400

    Adds road tile, ground tile, and moped constructors

--- game.js
+++ game.js
@@ -10,15 +10,31 @@ function Entity (w,h,m,xPos,yPos,img) {
 
 //Constructor for Tile Class
 function Tile (w,h,m,xPos,yPos,img,dir) {
-	Entity.call(this,w,h,m,xPos,yPos,img)
+	Entity.call(this,w,h,m,xPos,yPos,img);
 	this.direction = dir;
 } 
 
 //Constructor for Movable Class
 function Movable (w,h,m,xPos,yPos,img,xDir) {
-	Entity.call(this,w,h,m,xPos,yPos,img)
+	Entity.call(this,w,h,m,xPos,yPos,img);
 	this.xMove = xDir;
 } 
 
-var myEntity = new Tile(1,1,false,10,10,"image.png",-1);
+//Road Tile constructor
+function Road (x,y,dir) {
+	Tile.call(this,1,1,true,x,y,"",dir);
+	var mopeds = [];
+}
+
+//Ground Tile constructor
+function Ground (x,y) {
+	Tile.call(this,1,1,true,x,y,"",0);
+}
+
+//Moped constructor
+function Moped (x,y,dir) {
+	Movable.call(this,1,1,false,x,y,"",dir);
+}
+
+var myEntity = new Moped(1,1,-3)
 console.log(myEntity.height);
