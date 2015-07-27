456471d144f85e9f087c9479567a0adef13523f7
Kayleigh Migdol <kayleigh@migdol.net>
2015-07-27 15:23:08 -0400

    adds constructor for Tile class

--- game.js
+++ game.js
@@ -7,3 +7,10 @@ function Entity (w,h,m,xPos,yPos,img) {
 	this.y = yPos;
 	this.imageSource = img;
 }
+
+//Constructor for Tile Class
+function Tile (w,h,m,xPos,yPos,img,dir) {
+	Entity.call(this,w,h,m,xPos,yPos,img)
+	this.direction = dir;
+} 
+
